958ed28c7406394dca584fda2013c9c36b283bfe
NakamuraRyotaro <nakamura.ryotaro1121@gmail.com>
2026-07-20 15:45:52 +0900

    環境構築+gitignore追加

---
+++ backend/src/test/java/dev/nakamura/kakeibo_api/KakeiboApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nakamura.kakeibo_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakeiboApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
