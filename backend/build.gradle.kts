07a573397e8966e2cd5ed64b10b52b8ba44fe11c
NakamuraRyotaro <nakamura.ryotaro1121@gmail.com>
2026-07-24 13:59:38 +0900

    H2データベースの依存関係、ビルドおよび実行ステージ用のDockerfile、アプリケーションのスケルトン設定を追加

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-webmvc")
 	runtimeOnly("com.mysql:mysql-connector-j")
+	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-actuator-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-validation-test")
